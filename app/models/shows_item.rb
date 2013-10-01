fd60904e5c9c0fcef6b06cc2b7d43d6582fde048
Bennett Lin <bennettslin@gmail.com>
2013-09-30 19:48:30 -0700

    add seeds

--- app/models/shows_item.rb
+++ app/models/shows_item.rb
@@ -2,14 +2,17 @@ class ShowsItem < ActiveRecord::Base
 
   before_validation :smart_add_url_protocol
 
-  paginates_per 5
+  paginates_per 15
   validates_presence_of :name, :city, :state
 
   protected
 
   def smart_add_url_protocol
-    unless self.webpage == "" || self.webpage[/^http:\/\//] || self.webpage[/^https:\/\//]
-      self.webpage = 'http://' + self.webpage
+    if self.webpage.present?
+      unless self.webpage == "" || self.webpage[/^http:\/\//] ||
+        self.webpage[/^https:\/\//]
+        self.webpage = 'http://' + self.webpage
+      end
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,44 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-first_admins = ["the band", "Bennett"]
+first_admins = ["the band", "Bennett", "test admin"]
 
+count = 0
 first_admins.each do |name|
+  # only first two seeded admins are super admins
+  count < 2 ? super_status = true : super_status = false
   Admin.create(name: name, password: "test", password_confirmation: "test",
-    description: "Change this text. The password is 'test'.", super_admin: true)
+    description: "The password is 'test'. Change this text and the password!",
+    super_admin: super_status)
+  count += 1
 end
 
-first_albums = [["Yearling's Bobtail I", "2013-06-01 00:00:00"],
+past_albums = [["Yearling's Bobtail I", "2013-06-01 00:00:00"],
   ["Yearling's Bobtail II", "2013-06-01 00:00:00"],
-  ["Rosalind Franklin Meets Her Pepper's Ghost", "2012-03-01 00:00:00"]]
+  ["Rosalind Franklin Meets<br>Her Pepper's Ghost", "2012-03-01 00:00:00"]]
 
-first_albums.each do |album|
-  Album.create(title: album[0], date: album[1])
+past_albums.each do |album|
+  Album.create(title: album[0], date: album[1],
+    description: "Add description!")
+end
+
+ybi_songs = ["Didi", "Willy the Cocoa", "Dear Saskia", "Odin", "Ash Wednesday",
+  "Pchelka's Starry Journey", "M", "Good Night, Sita", "Grasshoppers Lie Heavy"]
+ybii_songs = ["Constellations!", "Look Back in Anger", "Garryowen",
+  "On a Golden Cord", "Kyon?", "Uncanny Valley Boy", "Cremated",
+  "Vegan Proclamation", "My 100,000th Dream"]
+rfmhpg_songs = ["Young Rosalind at the World's Fair", "All the Peppy Paulinas",
+  "Coffee in a Crucible", "Ruptures", "Photograph 51", "Unhatched", "Honest Jim",
+  "Rosalind's Mosaic Path", "Last Labours", "Whale Fall"]
+
+ybi_songs.each do |song|
+  Song.create(title: song, album_id: 1, lyrics: "Add lyrics!")
+end
+ybii_songs.each do |song|
+  Song.create(title: song, album_id: 2, lyrics: "Add lyrics!")
+end
+rfmhpg_songs.each do |song|
+  Song.create(title: song, album_id: 3, lyrics: "Add lyrics!")
 end
 
 first_news = [["Welcome to our website!",
@@ -27,3 +52,40 @@ first_news = [["Welcome to our website!",
 first_news.each do |news|
   NewsItem.create(heading: news[0], entry: news[1])
 end
+
+past_shows = [["Goodbye Blue Monday", "Brooklyn", "NY", "2008-10-27 00:00:00"],
+  ["Pete's Candy Store", "Williamsburg", "NY", "2008-10-13 00:00:00"],
+  ["Lucky Cat", "Williamsburg", "NY", "2008-10-03 00:00:00"],
+  ["Pete's Candy Store", "Williamsburg", "NY", "2008-05-11 00:00:00"],
+  ["Sputnik", "Williamsburg", "NY", "2008-04-27 00:00:00"],
+  ["R Bar", "New York", "NY", "2008-01-23 00:00:00"],
+  ["Luna Lounge", "Williamsburg", "NY", "2007-09-23 00:00:00"],
+  ["Pete's Candy Store", "Williamsburg", "NY", "2007-08-29 00:00:00"],
+  ["Desmond's", "New York", "NY", "2007-08-10 00:00:00"],
+  ["Luna Lounge", "Williamsburg", "NY", "2007-06-03 00:00:00"],
+  ["Pete's Candy Store", "Williamsburg", "NY", "2007-05-14 00:00:00"],
+  ["The Gig", "Hollywood", "CA", "2006-10-18 00:00:00"],
+  ["Sea Level Records", "Echo Park", "CA", "2006-07-15 00:00:00"],
+  ["Talking Stick", "Santa Monica", "CA", "2006-07-07 00:00:00"],
+  ["Silverlake Lounge", "Los Angeles", "CA", "2006-06-27 00:00:00"],
+  ["Knitting Factory", "Hollywood", "CA", "2006-05-14 00:00:00"],
+  ["The Gig", "Hollywood", "CA", "2006-05-02 00:00:00"]]
+
+past_shows.each do |show|
+  ShowsItem.create(name: show[0], city: show[1], state: show[2], date: show[3])
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
